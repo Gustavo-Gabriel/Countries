f0f2d97d676a635af6857390e4add386b3cc742c
Gustavo Anjos <gustavoanjos42@gmail.com>
2021-02-01 12:24:14 -0400

    feat: add CountryModel and CountryData

---
+++ Countries/Data/CountryData.swift
@@ -0,0 +1,18 @@
+//
+//  CountryData.swift
+//  Countries
+//
+//  Created by Gustavo Anjos on 01/02/21.
+//
+
+import SwiftUI
+
+let countriesData: [Country] = [
+    Country(title: "Argélia",
+            headline: "A Argélia está localizada no litoral norte da África, é banhada pelo Mediterrâneo.",
+            image: "argelia"),
+    
+    Country(title: "Argentina",
+            headline: "A Argentina é um país localizado no hemisfério sul ocidental, situado na América do Sul.",
+            image: "argentina")
+]
---
+++ Countries/Model/CountryModel.swift
@@ -0,0 +1,17 @@
+//
+//  Country.swift
+//  Countries
+//
+//  Created by Gustavo Anjos on 01/02/21.
+//
+
+import SwiftUI
+
+// MARK: - COUNTRIES DATA MODEL
+
+struct Country: Identifiable {
+    var id = UUID()
+    var title: String
+    var headline: String
+    var image: String
+}
