2bbc62b382fe97ee13f8b377e9076bd680aff71c
Gustavo Anjos <gustavoanjos42@gmail.com>
2021-02-01 12:11:53 -0400

    Initial Commit

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Gustavo Anjos on 01/02/21.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
